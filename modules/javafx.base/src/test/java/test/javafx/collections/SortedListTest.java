d94643f547da93df72d0117aa60405a0cc4f0744
Loay Ghreeb <loayahmed655@gmail.com>
2024-07-28 07:12:06 +0300

    Update SortedListTest.java

--- modules/javafx.base/src/test/java/test/javafx/collections/SortedListTest.java
+++ modules/javafx.base/src/test/java/test/javafx/collections/SortedListTest.java
@@ -34,6 +34,8 @@ import java.util.Collections;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import javafx.beans.Observable;
 import javafx.beans.property.SimpleObjectProperty;
